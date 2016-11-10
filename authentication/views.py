7b1151771ca99aa677aa7b3f7a690d71fff8976c
CokkocZateki <tenayamasai2@gmail.com>
2016-11-10 21:48:48 +0100

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -1,20 +1,16 @@
-from __future__ import unicode_literals
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from eveonline.managers import EveManager
-from eveonline.models import EveCharacter
-from authentication.models import AuthServicesInfo
-from authentication.forms import LoginForm, RegistrationForm
-from django.contrib.auth.models import User
-from django.contrib import messages
-from eve_sso.decorators import token_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.utils import translation
+
+from forms import LoginForm
+
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 def login_user(request):
     logger.debug("login_user called by user %s" % request.user)
@@ -28,92 +24,61 @@ def login_user(request):
                 if user.is_active:
                     logger.info("Successful login attempt from user %s" % user)
                     login(request, user)
-                    redirect_to = request.POST.get('next', request.GET.get('next', ''))
-                    if not redirect_to:
-                        redirect_to = 'auth_dashboard'
-                    return redirect(redirect_to)
+                    return HttpResponseRedirect("/dashboard/")
                 else:
                     logger.info("Login attempt failed for user %s: user marked inactive." % user)
-                    messages.warning(request, 'Your account has been disabled.')
             else:
                 logger.info("Failed login attempt: provided username %s" % form.cleaned_data['username'])
-                messages.error(request, 'Username/password invalid.')
-            return render(request, 'public/login.html', context={'form': form})
+
+            return render_to_response('public/login.html', {'form': form, 'error': True},
+                                      context_instance=RequestContext(request))
     else:
         logger.debug("Providing new login form.")
         form = LoginForm()
 
-    return render(request, 'public/login.html', context={'form': form})
+    return render_to_response('public/login.html', {'form': form}, context_instance=RequestContext(request))
 
 
 def logout_user(request):
     logger.debug("logout_user called by user %s" % request.user)
-    temp_user = request.user
+    logoutUser = request.user
     logout(request)
-    logger.info("Successful logout for user %s" % temp_user)
-    return redirect("auth_index")
+    logger.info("Successful logout for user %s" % logoutUser)
+    return HttpResponseRedirect("/")
+
+    
+def redirect_to_sso(request):
+    eve_sso = EvESSO(request)
+    eve_sso_redirect_uri = eve_sso.generate_redirect_uri(eve_sso.generate_state())
+    if eve_sso_redirect_uri is not None:
+        return HttpResponseRedirect(eve_sso_redirect_uri)
+
+    return HttpResponseBadRequest('Could not process sso request.')
 
 
-def register_user_view(request):
-    logger.debug("register_user_view called by user %s" % request.user)
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        logger.debug("Request type POST contains form valid: %s" % form.is_valid())
-        if form.is_valid():
+def login_sso(request):
+    eve_sso = EvESSO(request)
+    if eve_sso.has_valid_state() is False:
+        return HttpResponseBadRequest('Could not authenticate, incorrect state parameter received')
 
-            if not User.objects.filter(username=form.cleaned_data['username']).exists():
-                user = User.objects.create_user(form.cleaned_data['username'],
-                                                form.cleaned_data['email'], form.cleaned_data['password'])
+    if eve_sso.verify_auth_code() is False:
+        return HttpResponseBadRequest('EvE Auth Token could not be verified, please try again.')
 
-                user.save()
-                logger.info("Created new user %s" % user)
-                messages.warning(request, 'Add an API key to set up your account.')
-                return redirect("auth_dashboard")
+    char_info = eve_sso.obtain_char_info()
+    if char_info is False:
+        return HttpResponseBadRequest('A problem occurred, while verifying your barer token.')
 
-            else:
-                logger.error("Unable to register new user: username %s already exists." % form.cleaned_data['username'])
-                return render(request, 'public/register.html', context={'form': form, 'error': True})
-        else:
-            logger.debug("Registration form invalid. Returning for user %s to make corrections." % request.user)
+    if 'CharacterID' not in char_info:
+        return HttpResponseBadRequest('No valid Character info could be found.')
 
-    else:
-        logger.debug("Returning blank registration form.")
-        form = RegistrationForm()
+    character_id = char_info['CharacterID']
+    character_name = char_info['CharacterName']
 
-    return render(request, 'public/register.html', context={'form': form})
+    if not User.objects.filter(username=character_id).exists():
+        user = User.objects.create_user(character_id, None, character_name)
+        user.save()
 
+    user = authenticate(username=character_id, password=character_name)
+    login(request, user)
 
-def index_view(request):
-    logger.debug("index_view called by user %s" % request.user)
-    return render(request, 'public/index.html')
-
-
-@login_required
-def dashboard_view(request):
-    logger.debug("dashboard_view called by user %s" % request.user)
-    render_items = {'characters': EveManager.get_characters_by_owner_id(request.user.id),
-                    'authinfo': AuthServicesInfo.objects.get_or_create(user=request.user)[0]}
-    return render(request, 'registered/dashboard.html', context=render_items)
-
-
-@login_required
-def help_view(request):
-    logger.debug("help_view called by user %s" % request.user)
-    return render(request, 'registered/help.html')
-
-@token_required(new=True)
-def sso_login(request, tokens=[]):
-    token = tokens[0]
-    try:
-        char = EveCharacter.objects.get(character_id=token.character_id)
-        if char.user:
-            if char.user.is_active:
-                login(request, char.user)
-                return redirect(dashboard_view)
-            else:
-                messages.error(request, 'Your account has been disabled.')
-        else:
-            messages.warning(request, 'Authenticated character has no owning account. Please log in with username and password.')
-    except EveCharacter.DoesNotExist:
-        messages.error(request, 'No account exists with the authenticated character. Please create an account first.')
-    return redirect(login_user)
+    return HttpResponseRedirect('/dashboard')
