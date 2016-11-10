8824b878d2e0cc02355b9af9245b512b9066fcc9
CokkocZateki <tenayamasai2@gmail.com>
2016-11-10 22:20:49 +0100

    Update backends.py

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,15 +1,23 @@
+import re
+
 from django.contrib.auth.models import User
 
 from eveonline.models import EveCharacter
 
 
 class EvESsoBackend(object):
-    def authenticate(self, character_id=None, character_name=None):
+    def authenticate(self, character_id=None, character_name=None, character_owner_hash=None):
         try:
             eve_character = EveCharacter.objects.get(character_id=character_id)
             return eve_character.user
         except EveCharacter.DoesNotExist:
-            return None
+            try:
+                user = User.objects.get(username=character_owner_hash)
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=character_owner_hash, password=character_owner_hash)
+                user.first_name = character_name
+                user.save()
+            return user
 
     def get_user(self, user_id):
         try:
